26d0e1550eae15502c221120317bb7510b6e6e1a
pablomds <moraisdesouzapablo@gmail.com>
2025-02-25 19:52:12 +0100

    adding todo-list to routes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 // import Home from '@/views/Home.vue';
 import Login from '@/views/Login.vue';
 import Register from '@/views/Register.vue';
+import TodoList from '@/views/TodoList.vue';
 // import Login from '../views/Login.vue';
 
 const routes = [
   { path: '/', component: Register },
-  { path: '/login', component: Login }
+  { path: '/login', component: Login },
+  { path: '/todo-list', component: TodoList },
 ];
 
 export default createRouter({
