bb67cc581a577ffa8024aaa63d5fa1e6402079ed
pablomds <moraisdesouzapablo@gmail.com>
2025-02-27 13:38:56 +0100

    user routes crud

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,9 +1,43 @@
-import supabase from '../config/supabase.ts';
+import supabase from '../config/supabase.js';
+import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify'
+import { $ref } from '../schemas/user.schemas.js';
+import { createUser, getUsers, login, logout } from '../controllers/user.controller.js';
 
-export default async function userRoutes(fastify, options) {
-  fastify.get('/users', async (request, reply) => {
-    const { data, error } = await supabase.from('users').select('*');
-    if (error) return reply.status(500).send(error);
-    return data;
-  });
-}
+export default async function userRoutes(fastify: FastifyInstance) {
+
+  fastify.get(
+    '/',
+    {
+      preHandler: [fastify.authenticate],
+    },
+    getUsers,
+  )
+
+  fastify.post(
+    '/register',
+    {
+      schema: {
+        body: $ref('createUserSchema'),
+        response: {
+          201: $ref('createUserResponseSchema'),
+        },
+      },
+    },
+    createUser,
+  )
+
+  fastify.post(
+    '/login',
+    {
+      schema: {
+        body: $ref('loginSchema'),
+        response: {
+          201: $ref('loginResponseSchema'),
+        },
+      },
+    },
+    login,
+  )
+
+  fastify.delete('/logout', { preHandler: [fastify.authenticate] }, logout);
+};
