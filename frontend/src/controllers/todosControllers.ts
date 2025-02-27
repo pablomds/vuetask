384c5d9b230a066f18cc4fb213fbd349745a086a
pablomds <moraisdesouzapablo@gmail.com>
2025-02-28 00:09:39 +0100

    todosControllers file

---
+++ frontend/src/controllers/todosControllers.ts
@@ -0,0 +1,28 @@
+import axiosInstance from "@/utils/axios";
+
+export const getUserTodos = async () => {
+    try {
+        const { data: todos } = await axiosInstance.get('/todos');
+        return todos
+    } catch (error) {
+        console.log('error on fetchUserTodos()', error)
+        return error
+    }
+};
+
+export const createTodo = async (data: any):Promise<number | void> => {
+    try {
+        const { data: todo } = await axiosInstance.post('/todos', data);
+        return todo.id
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const updateTodo = async (data: any): Promise<void | unknown> => {
+    try {
+        await axiosInstance.patch('/todos/update', data)
+    } catch (error) {
+        return error
+    }
+};
