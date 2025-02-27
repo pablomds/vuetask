77ab842cc279af8cfcb7f74a0983f1c617a3a072
pablomds <moraisdesouzapablo@gmail.com>
2025-02-27 15:50:05 +0100

    using proxy inside vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 import svgLoader from 'vite-svg-loader';
+
+const env = loadEnv(
+  'all',
+  process.cwd()
+);
 
 export default defineConfig({
   plugins: [vue(), tailwindcss(), svgLoader()],
   resolve: {
     alias: {
-      '@': '/src'  // ✅ Ensure the alias is correct
+      '@': '/src' 
     }
-  }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: env.VITE_API_BASE_URL, // Your Fastify server URL
+        changeOrigin: true,
+        secure: false, // Set to true if using HTTPS
+      },
+    },
+  },
 });
