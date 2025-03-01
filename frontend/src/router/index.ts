ab0d9e39c651839bc0e43438366f72666a6b81f6
pablomds <moraisdesouzapablo@gmail.com>
2025-03-01 09:56:54 +0100

    verify if user is connected

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,17 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { useRouter } from 'vue-router';
 // import Home from '@/views/Home.vue';
 import Login from '@/views/Login.vue';
 import Register from '@/views/Register.vue';
 import TodoList from '@/views/TodoList.vue';
+import axiosInstance from '@/utils/axios';
 // import Login from '../views/Login.vue';
+
 
 const routes = [
   { path: '/', component: Register },
   { path: '/login', component: Login },
-  { path: '/todo-list', component: TodoList },
+  { path: '/todo-list', component: TodoList, meta: { requiresAuth: true } },
 ];
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes
 });
+
+router.beforeEach(async (to, from, next) => {
+  if (to.meta.requiresAuth) {
+    try {
+      const response = await axiosInstance.get('/users/me');
+
+      if (!response.statusText) {
+        throw new Error('Not authenticated');
+      }
+
+      next(); 
+
+    } catch (error) {
+      console.warn('User not authenticated, redirecting to login');
+      next('/login');
+    }
+  } else {
+    next();
+  }
+});
+
+export default router
