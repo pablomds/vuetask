a21d76530e67e07bdab2ba3d66ac5fc18fda3672
pablomds <moraisdesouzapablo@gmail.com>
2025-03-02 16:59:31 +0100

    rewriting base url

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,8 +1,12 @@
 // src/utils/axios.js
 import axios from 'axios';
 
+const baseUrl = import.meta.env.VITE_NODE_ENV === 'production' 
+  ? `${import.meta.env.VITE_API_BASE_URL}/api` 
+  : '/api';
+
 const axiosInstance = axios.create({
-  baseURL: `/api`, // Replace with your Fastify server URL
+  baseURL: baseUrl,
   timeout: 5000,
   withCredentials: true,
   headers: {'Content-Type': 'application/json'},
