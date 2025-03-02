852ea69eb5a8a4470c678a605351b87a0fa97682
pablomds <moraisdesouzapablo@gmail.com>
2025-03-02 19:36:19 +0100

    making configuration for production

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -71,7 +71,7 @@ export async function login(
       path: '/',
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production', // Set to true in production
-      sameSite: 'none',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       expires: new Date(Date.now() + 86400 * 1000),
       domain: process.env.NODE_ENV === 'production' ? 'vuetask.onrender.com' : undefined,
     })
