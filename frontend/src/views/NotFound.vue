401cbd94419e62ed1505aade42e82d666b2366a6
pablomds <moraisdesouzapablo@gmail.com>
2025-03-02 22:14:24 +0100

    page not found if there's no route match

---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+const goTo = () => router.push('/login')
+</script>
+
+<template>
+    <div class="w-screen h-screen overflow-hidden inset-0 flex flex-col justify-center items-center gap-y-8">
+        <div class="text-2xl text-red-500">Page Not Found</div>
+        <button @click="goTo()" class="bg-[#323232] text-white px-10 py-[10px] rounded-[10px] shadow-md shadow-primary-black/50 text-base cursor-pointer">Go back to login</button>
+    </div>
+</template>
