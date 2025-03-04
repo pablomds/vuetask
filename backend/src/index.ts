928bf765b9629e9f8c392326fce0bc80b1d0da89
pablomds <moraisdesouzapablo@gmail.com>
2025-03-04 23:35:07 +0100

    adding patch method

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ const fastify = Fastify({ logger: true });
 fastify.register(cors, {
   origin: process.env.FRONTEND_BASE_URL ? process.env.FRONTEND_BASE_URL : true ,
   credentials: true, 
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT','PATCH' , 'DELETE', 'OPTIONS'],
   allowedHeaders: [
     'Content-Type', 
     'Authorization', 
