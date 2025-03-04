7dd4af514f97e9bce46f36db2df083a67a8d4c77
pablomds <moraisdesouzapablo@gmail.com>
2025-03-04 23:03:28 +0100

    puttin in place domain undefined if its not prod

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -74,6 +74,7 @@ export async function login(
       secure: process.env.NODE_ENV === 'production', // Set to true in production
       sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
       expires: new Date(Date.now() + 86400 * 1000),
+      domain: process.env.NODE_ENV === 'production' ? '.vuetask.onrender.com' : undefined
     })
   
     return { accessToken: token }
